2575c78d7d8040d5b90c1887434287bc6f36d5af
Jonathan Guaynora <guaynorajon@gmail.com>
2025-07-02 07:17:20 -0500

    fix: fix android implementation

--- android/src/main/java/com/cookiesmanager/CookiesManagerPackage.kt
+++ android/src/main/java/com/cookiesmanager/CookiesManagerPackage.kt
@@ -1,33 +1,17 @@
 package com.cookiesmanager
 
-import com.facebook.react.BaseReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReactModuleInfo
-import com.facebook.react.module.model.ReactModuleInfoProvider
-import java.util.HashMap
+import com.facebook.react.uimanager.ViewManager
 
-class CookiesManagerPackage : BaseReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return if (name == CookiesManagerModule.NAME) {
-      CookiesManagerModule(reactContext)
-    } else {
-      null
-    }
+class CookiesManagerPackage : ReactPackage {
+
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(CookiesManagerModule(reactContext))
   }
 
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return ReactModuleInfoProvider {
-      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
-      moduleInfos[CookiesManagerModule.NAME] = ReactModuleInfo(
-        CookiesManagerModule.NAME,
-        CookiesManagerModule.NAME,
-        false,  // canOverrideExistingModule
-        false,  // needsEagerInit
-        false,  // isCxxModule
-        true // isTurboModule
-      )
-      moduleInfos
-    }
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
   }
 }
